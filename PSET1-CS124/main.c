b2af4f0ab42780c7f907fd611136499023d6a791
Samuel Lam <samuelkinlam@gmail.com>
2017-01-30 12:11:13 -0500

    Initial Commit

---
+++ PSET1-CS124/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PSET1-CS124
+//
+//  Created by Samuel K. Lam on 1/30/17.
+//  Copyright © 2017 Samuel K. Lam. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
